6f9c9b9a849a9cea2946c3c48c7e3334800754cb
Amauri Rodrigues <amauriaureo@gmail.com>
2023-02-21 19:39:23 -0300

    usuario insere categoria

---
+++ cli_todo/category/create_category.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conexao = sqlite3.connect('../todo.sqlite3')
+
+print("Vamos criar uma categora?")
+nome = input("Insira o nome da categoria: ")
+print("Categoria inserida com sucesso!")
+valores = [nome]
+sql_inserir_categoria = 'insert into categoria (nome) values (?)'
+cursor = conexao.cursor()
+cursor.execute(sql_inserir_categoria, valores)
+conexao.commit()
+conexao.close()
