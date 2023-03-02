a8061bb15122fe702e1abdfc12dbc83c737a69bb
Amauri Rodrigues <amauriaureo@gmail.com>
2023-03-02 10:12:11 -0300

    importando de funcoes

--- cli_todo/tasks/delete_task.py
+++ cli_todo/tasks/delete_task.py
@@ -1,18 +1,3 @@
-import sqlite3
+import functions as fun
 
-conexao = sqlite3.connect('../todo.sqlite3')
-cursor = conexao.cursor()
-
-print("Tarefas disponíveis: ")
-tarefas_disponiveis = "select * from tarefa"
-consulta = cursor.execute(tarefas_disponiveis)
-for resultado in consulta:
-    print(resultado)
-
-deletar_tarefa = input("Digite o ID da tarefa que deseja excluir: ")
-print(f'{resultado[1]} removido com sucesso.')
-valores = [deletar_tarefa]
-sql = 'delete from tarefa where id = ?'
-cursor.execute(sql, valores)
-conexao.commit()
-conexao.close()
+fun.deletar_tarefa()
